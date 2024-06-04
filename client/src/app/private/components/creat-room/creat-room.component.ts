9212566b87e6a226a4576af0af8a4d772db2dfed
zhihanzhao <zhao.zhiha@northeastern.edu>
2024-06-03 17:14:02 -0700

    Create room page

---
+++ client/src/app/private/components/creat-room/creat-room.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-creat-room',
+  templateUrl: './creat-room.component.html',
+  styleUrls: ['./creat-room.component.scss']
+})
+export class CreatRoomComponent {
+
+}
--- client/src/app/private/private-routing.module.ts
+++ client/src/app/private/private-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { HomepageComponent } from "./components/homepage/homepage.component";
+import { CreatRoomComponent } from "./components/creat-room/creat-room.component";
 
 const routes: Routes = [
     {
         path: 'home', component: HomepageComponent
-    }
+    },
+    {
+        path: 'create-room', component: CreatRoomComponent
+    },
 ];
 
 @NgModule({
